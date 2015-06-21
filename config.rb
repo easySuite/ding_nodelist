a774dc889c9bf92e84eeb148d0ac7c6fe9ab15cc
Anna Caraman <ac@inleadmedia.com>
2015-06-21 15:40:57 +0300

    DDBEASY-664 - Ding nodelist styles.

--- config.rb
+++ config.rb
@@ -1,25 +1,27 @@
-require 'compass/import-once/activate'
-# Require any additional compass plugins here.
+# First, require any additional compass plugins installed on your system.
+require 'zen-grids'
+require 'susy'
+# require 'breakpoint'
 
-# Set this to the root of your project when deployed:
+# Location of the your project's resources.
+css_dir         = "css"
+sass_dir        = "sass"
+extensions_dir  = "sass-extensions"
+images_dir      = "images"
+javascripts_dir = "js"
+
+# Set this to the root of your project. All resource locations above are
+# considered to be relative to this path.
 http_path = "/"
-css_dir = "css"
-sass_dir = "sass"
-images_dir = "images"
-javascripts_dir = "javascripts"
+
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
+output_style = :expanded
 
-# To enable relative paths to assets via compass helper functions. Uncomment:
-# relative_assets = true
+# Pass options to sass. For development, we turn on the FireSass-compatible
+# debug_info if the firesass config variable above is true.
+sass_options =  {:debug_info => false}
 
-# To disable debugging comments that display the original location of your selectors. Uncomment:
+# Ignore line comments when compiling, in order to avoid confusion.
 line_comments = false
-
-
-# If you prefer the indented syntax, you might want to regenerate this
-# project again passing --syntax sass, or you can uncomment this:
-# preferred_syntax = :sass
-# and then run:
-# sass-convert -R --from scss --to sass sass scss && rm -rf sass && mv scss sass
