2eaec346d561d418ed661e689ccf8039a7583c0b
Alex Damaschin <ad@peytz.dk>
2016-05-26 18:24:58 +0300

    EASYOPAC-100 - Getting nodelist taxonomy display look closer to site's look.

--- templates/ding_nodelist_widget_taxonomy.tpl.php
+++ templates/ding_nodelist_widget_taxonomy.tpl.php
@@ -17,11 +17,25 @@
     <?php endif; ?>
     <div class="ding_nodelist-items">
       <?php
+      $groups = array();
       foreach ($items as $node) {
-        print theme($node->item_template, array(
-          'item' => $node,
-          'conf' => $conf,
-        ));
+        $ed = date('d-m-Y', _ding_nodelist_get_event_date($node));
+        if (!isset($groups[$ed])) {
+          $groups[$ed] = array($node);
+        }
+        else {
+          $groups[$ed][] = $node;
+        }
+      }
+
+      foreach ($groups as $group) {
+        $group[0]->has_header = TRUE;
+        foreach ($group as $k => $v) {
+          print theme($group[$k]->item_template, array(
+            'item' => $group[$k],
+            'conf' => $conf,
+          ));
+        }
       }
       ?>
     </div>
