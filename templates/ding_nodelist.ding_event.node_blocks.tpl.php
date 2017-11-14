1f4345ca93342ec26b9797ea4226e1f81624a0f2
Alex Damaschin <ad@peytz.dk>
2017-11-14 11:01:57 +0200

    EASYOPAC-633 - Adding missing fields to displays.

--- templates/ding_nodelist.ding_event.node_blocks.tpl.php
+++ templates/ding_nodelist.ding_event.node_blocks.tpl.php
@@ -11,14 +11,17 @@ $library = field_view_field('node', $item, 'og_group_ref', 'default');
 $price = field_view_field('node', $item, 'field_ding_event_price', 'default');
 $lead = field_get_items('node', $item, 'field_' . $item->type . '_lead');
 $teaser = field_get_items('node', $item, 'field_' . $item->type . '_body');
+$image_field = 'field_' . $item->type . '_list_image';
+$image = _ding_nodelist_get_dams_image_info($item, $image_field);
 ?>
 <div class="nb-item <?php print $item->type; ?>">
-  <div class="nb-inner">
+    <div class="nb-inner">
     <div class="nb-event-date">
       <?php print t(date('d. M.', $event_date)); ?>
     </div>
     <h3><?php print l($item->title, 'node/' . $item->nid); ?></h3>
-    <div class="nb-event-summary">
+      <?php print theme('image_style', array_merge($image, array('style_name' => $conf['image_style']))); ?>
+      <div class="nb-event-summary">
       <?php
       if (isset($lead[0]['safe_value'])) {
         print strip_tags($lead[0]['safe_value']);
--- templates/ding_nodelist.ding_news.node_blocks.tpl.php
+++ templates/ding_nodelist.ding_news.node_blocks.tpl.php
@@ -5,11 +5,22 @@
  */
 $lead = field_get_items('node', $item, 'field_' . $item->type . '_lead');
 $teaser = field_get_items('node', $item, 'field_' . $item->type . '_body');
+$image_field = 'field_' . $item->type . '_list_image';
+$image = _ding_nodelist_get_dams_image_info($item, $image_field);
+$category = field_view_field('node', $item, 'field_ding_news_category', 'teaser');
+$news_date = date('d.m.y', $item->created);
+if ($item->created < $item->changed) {
+  $news_date = date('d.m.y', $item->changed);
+}
 ?>
 <div class="nb-item <?php print $item->type; ?>">
   <div class="nb-inner">
     <h3><?php print l($item->title, 'node/' . $item->nid); ?></h3>
-    <div class="nb-event-summary">
+    <?php print theme('image_style', array_merge($image, array('style_name' => $conf['image_style']))); ?>
+    <?php print drupal_render($category); ?>
+      <?php print $news_date; ?>
+
+      <div class="nb-event-summary">
       <?php
       if (isset($lead[0]['safe_value'])) {
         print strip_tags($lead[0]['safe_value']);
