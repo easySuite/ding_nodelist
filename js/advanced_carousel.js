ff0de73ef661b5ef3386d4489dd22396acafafc1
Alex <bozeak@gmail.com>
2019-10-29 16:05:16 +0200

    EASYOPAC-1112 - Kultur: New nodelist widget.

---
+++ js/advanced_carousel.js
@@ -0,0 +1,42 @@
+(function ($) {
+  'use strict';
+
+  Drupal.behaviors.nodelist_advanced_carousel = {
+    attach: function (context, settings) {
+      $('.ding_nodelist-advanced-carousel', context).slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        fade: true,
+        asNavFor: '.slider-nav'
+      });
+      $('.slider-nav', context).slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        asNavFor: '.ding_nodelist-advanced-carousel',
+        focusOnSelect: true,
+        infinite: false,
+        arrows: false,
+      });
+
+      // Slide indicator
+      var $total = $('.carousel-controls__indicator .indicator--total');
+      var $current = $('.carousel-controls__indicator .indicator--current');
+      $('.ding_nodelist-advanced-carousel', context).on('init afterChange', function (event, slick, currentSlide, nextSlide) {
+        var curslide = slick.currentSlide + 1;
+        $current.text('0' + curslide);
+        $total.text('0' + slick.slideCount);
+      });
+
+      $('.carousel-controls__next', context).click(function () {
+        $('.ding_nodelist-advanced-carousel', context).slick('slickNext');
+      });
+
+      // Prev slide button
+      $('.carousel-controls__prev', context).click(function () {
+        $('.ding_nodelist-advanced-carousel', context).slick('slickPrev');
+      });
+
+    }
+  }
+})(jQuery, Drupal);
